9eac5cfbc702702c6ce68d4140bcbabf619923b7
Krishnas-world <krishnapallan128@gmail.com>
2024-05-05 21:57:30 +0530

    completed the home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,63 +1,85 @@
 import React from "react";
 import { Navbar, Button } from "flowbite-react";
-import { Link, useLocation} from "react-router-dom"; //! You go to the page without refreshing the page
-import { AiOutlineSearch } from "react-icons/ai"; //! Added a search icon
+import { Link, useLocation } from "react-router-dom"; //! You go to the page without refreshing the page
+// import { AiOutlineSearch } from "react-icons/ai"; //! Added a search icon
 import { FaMoon } from "react-icons/fa";
 //! Header component
 
 const Header = () => {
-
   const path = useLocation().pathname; //! This is used to get the current path of the page so we use the component useLoc from react-router-dom
 
-
   return (
-    <Navbar className=" border-b-2">
+    <Navbar className=" bg-black bg-opacity-40">
       <Link
         to="/"
-        className=" self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white flex items-center" 
+        className=" self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white flex items-center"
       >
         <span>
-          <img src="../../public/favicon.jpeg" alt="Aetherial Icon" className="h-8" />
+          <img
+            src="../../public/favicon.jpeg"
+            alt="Aetherial Icon"
+            className="h-8"
+          />
         </span>
-        <span className="ml-2" style={{ fontFamily: 'Montserrat'}}>AETHERIAL</span> {/* Text added here */}
+        <span className="ml-2" style={{ fontFamily: "Montserrat" }}>
+          AETHERIAL
+        </span>{" "}
+        {/* Text added here */}
       </Link>
-      {/* //!added a search button here */}
-      <Button className="w-12 h-10 lg:hidden" color="red" pill> {/* //! added pill because it rounds the button */}
-        <AiOutlineSearch />
-      </Button>
 
       <div className="flex gap-2 md:order-2">
         {/* //! Added a button to change the modes of the page */}
-        <Button className="w-12 h-10 hidden sm:inline" color="grey" pill>
+        <Button className="w-14 h-10 hidden sm:inline" color="black" pill>
           <FaMoon /> {/* Added a moon icon to change the theme*/}
         </Button>
-
         <Link to="/signin">
-          <Button color="red" pill>
-            Sign In
+          <Button color="" pill>
+            <span style={{ fontSize: 19 }}>Sign In</span>
           </Button>
         </Link>
-
         <Navbar.Toggle /> {/*//! Adds a hamburger menu for the phone view */}
       </div>
 
       {/* Adding external navbar links */}
 
-        <Navbar.Collapse> {/* //! without this, navbar content comes as bullets. (li tag) */}
-          
-          {/* //! As 2 <a> links cant be there under one navbar prop we change it as "div" */}
-          <Navbar.Link active={path === '/'} as={'div'}> {/* We add this to show which of the page is active */}
-            <Link to="/">Home</Link> {/* //! Link tag helps to traverse one to next component */}
-          </Navbar.Link>
-          <Navbar.Link active={path === '/About'} as={'div'}>
-            <Link to="/About">About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/Projects'}as={'div'}>
-            <Link to="/Projects">Services</Link>
-          </Navbar.Link>
-
-
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        {" "}
+        {/* //! without this, navbar content comes as bullets. (li tag) */}
+        {/* //! As 2 <a> links cant be there under one navbar prop we change it as "div" */}
+        <Navbar.Link active={path === "/"} as={"div"}> {/* We add this to show which of the page is active */}
+          <Link
+            to="/"
+            className={`text-black ${
+              path === "/" ? "font-bold" : ""
+            } hover:text-gray-200`}
+            style={{ fontSize: 19 }}
+          >
+            Home
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/About"} as={"div"}>
+          <Link
+            to="/About"
+            className={`text-black ${
+              path === "/About" ? "font-bold" : ""
+            } hover:text-gray-200`}
+            style={{ fontSize: 19 }}
+          >
+            About
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/Projects"} as={"div"}>
+          <Link
+            to="/Projects"
+            className={`text-black ${
+              path === "/Projects" ? "font-bold" : ""
+            } hover:text-gray-200`}
+            style={{ fontSize: 19 }}
+          >
+            Services
+          </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
-    <div className='bg-black h-4/5 flex justify-center'>
-      <h1 className='font-bold text-white text-center'>AETHERIAL</h1>
+    <div className='min-h-screen flex justify-center items-center relative bg-gradient-to-b from-opacity-50 to-black' style={{backgroundImage: "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.985)), url('/helmet.jpeg')", backgroundSize: 'cover'}}>
+      {/* Content */}
+      <div className="z-20 text-center relative">
+        <h1 className='font-bold text-white text-8xl  text-s letter-spacing tracking-wide' style={{fontSize: 100}}><b>AETHERIAL</b></h1>
+        <p className="text-center text-2xl text-gray-400 m-3"><b>You Elevate Your Ride, We Safeguard Your Journey</b></p>
+        <p className="text-white font-medium text-center m-12 text-2xl px-14">
+          Aetherial is an innovative smart helmet designed to enhance road
+          safety and provide advanced protection for riders. The helmet
+          incorporates cutting-edge technology to address key safety concerns,
+          including accident detection, emergency response, and preventive
+          measures to ensure that riders wear their helmets.
+        </p>
+        <div className="flex justify-center">
+          <Button
+            variant='outlined'
+            sx={{
+              color: 'white',
+              paddingLeft: '2em',
+              paddingRight: '2em',
+              paddingTop: '1em',
+              paddingBottom: '1em',
+              borderColor: 'white',
+              fontFamily: 'Montserrat',
+              fontSize: 17,
+              '&:hover': {
+                borderColor: 'white',
+                backgroundColor: 'rgba(255,255,255,0.1)',
+              },
+              '& #change': {
+                color: 'white',
+              },
+            }}
+          >
+            <span id='change'>Login</span>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
