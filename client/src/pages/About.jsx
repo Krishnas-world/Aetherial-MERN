170281d34b613c47b25d324ba20ff4339ca1a777
Krishnas-world <krishnapallan128@gmail.com>
2024-05-06 12:39:14 +0530

    some changes basic in about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,14 +7,14 @@ export default function About() {
         <div className="container">
           <div className="row align-center">
             <div className="col-md-12 text-center"><br/>
-              <h2 className='section-title text-white text-9xl text-center'>ABOUT US</h2><br/>
+              <h2 className='section-title text-white text-9xl text-center' style={{font:'Montserrat'}}>ABOUT US</h2><br/>
               <p className='section-subtitle text-white text-center'>Welcome to AETHERIAL, where innovation meets safety on the open road. We are more than just a helmet; we are a commitment to the well-being of riders. AETHERIAL is a cutting-edge helmet and technology product meticulously crafted to redefine the standards of rider safety.At AETHERIAL, our vision is clear. To provide an unparalleled level of safety for riders who navigate the bustling streets and winding roads. We understand the risks associated with riding, and we are on a mission to eliminate preventable accidents caused by negligence. Our goal is to make every journey on two wheels not just an exhilarating adventure but a safe and secure experience</p>
             </div>
             <br/>
             <div className='flex flex-row items-start gap-5'>
             <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
               <div className='team-item '>
-                <img className='team-img rounded-full' alt='picture'/>
+                <img src='../../public/navneet-min.jpeg'className='team-img rounded-full' alt='picture'/>
                 <h3>Anirudha Udupa</h3>
                 <div className='team-info'>
                   <p>Team Lead</p>
@@ -35,7 +35,7 @@ export default function About() {
             <br/>
             <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
               <div className='team-item'>
-                <img src='../' className='team-img' alt='picture'/>
+                <img src='../../public/Anirudh-min.jpeg' className='team-img' alt='picture'/>
                 <h3>Anirudha Udupa</h3>
                 <div className='team-info'>
                   <p>Team Lead</p>
@@ -56,8 +56,8 @@ export default function About() {
             <br/>
             <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
               <div className='team-item'>
-                <img src='../' className='team-img' alt='picture'/>
-                <h3>Anirudha Udupa</h3>
+                <img src='../../public/logo.jpg' className='team-img' alt='picture'/>
+                <h3>Krishna H Pallan</h3>
                 <div className='team-info'>
                   <p>Team Lead</p>
                   <ul className='team-icon display: inline-flex flex-row'>
@@ -76,7 +76,7 @@ export default function About() {
             </div>
             <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
               <div className='team-item'>
-                <img src='../' className='team-img' alt='picture'/>
+                <img src='../../public/jonathan.jpg' className='team-img' alt='picture'/>
                 <h3>Anirudha Udupa</h3>
                 <div className='team-info'>
                   <p>Team Lead</p>
