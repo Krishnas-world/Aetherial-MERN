5e6b435a1ded485db8e81ca03d1787bd41055993
Krishnas-world <krishnapallan128@gmail.com>
2024-05-06 20:23:18 +0530

    changed the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,104 +1,9 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Global } from "@emotion/react";
-
+import ReactDOM from 'react-dom'
 export default function About() {
   return (
-<<<<<<< HEAD
-    <div>
-      <section className="bg-black">
-        <div className="container">
-          <div className="row align-center">
-            <div className="col-md-12 text-center"><br/>
-              <h2 className='section-title text-white text-9xl text-center' style={{font:'Montserrat'}}>ABOUT US</h2><br/>
-              <p className='section-subtitle text-white text-center'>Welcome to AETHERIAL, where innovation meets safety on the open road. We are more than just a helmet; we are a commitment to the well-being of riders. AETHERIAL is a cutting-edge helmet and technology product meticulously crafted to redefine the standards of rider safety.At AETHERIAL, our vision is clear. To provide an unparalleled level of safety for riders who navigate the bustling streets and winding roads. We understand the risks associated with riding, and we are on a mission to eliminate preventable accidents caused by negligence. Our goal is to make every journey on two wheels not just an exhilarating adventure but a safe and secure experience</p>
-            </div>
-            <br/>
-            <div className='flex flex-row items-start gap-5'>
-            <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
-              <div className='team-item '>
-                <img src='../../public/navneet-min.jpeg'className='team-img rounded-full' alt='picture'/>
-                <h3>Anirudha Udupa</h3>
-                <div className='team-info'>
-                  <p>Team Lead</p>
-                  <ul className='team-icon rounded-lg flex-1'>
-                    <li><a href='#' className='instagram '>
-                    <iframe className='fa fa-instagram'></iframe>
-                    </a></li>
-                    <li><a href='#' className='github'>
-                    <iframe className='fa fa-github'></iframe>
-                    </a></li>
-                    <li><a href='#'className='linkedin'>
-                    <iframe className='fa fa-linkedin'></iframe>
-                    </a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <br/>
-            <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
-              <div className='team-item'>
-                <img src='../../public/Anirudh-min.jpeg' className='team-img' alt='picture'/>
-                <h3>Anirudha Udupa</h3>
-                <div className='team-info'>
-                  <p>Team Lead</p>
-                  <ul className='team-icon'>
-                    <li><a href='#' className='instagram'>
-                    <iframe className='fa fa-instagram'></iframe>
-                    </a></li>
-                    <li><a href='#' className='github'>
-                    <iframe className='fa fa-github'></iframe>
-                    </a></li>
-                    <li><a href='#'className='linkedin'>
-                    <iframe className='fa fa-linkedin'></iframe>
-                    </a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <br/>
-            <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
-              <div className='team-item'>
-                <img src='../../public/logo.jpg' className='team-img' alt='picture'/>
-                <h3>Krishna H Pallan</h3>
-                <div className='team-info'>
-                  <p>Team Lead</p>
-                  <ul className='team-icon display: inline-flex flex-row'>
-                    <li><a href='#' className='instagram'>
-                    <iframe className='fa fa-instagram'></iframe>
-                    </a></li>
-                    <li><a href='#' className='github'>
-                    <iframe className='fa fa-github'></iframe>
-                    </a></li>
-                    <li><a href='#'className='linkedin'>
-                    <iframe className='fa fa-linkedin'></iframe>
-                    </a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className='col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 size-[20rem] rounded-lg '>
-              <div className='team-item'>
-                <img src='../../public/jonathan.jpg' className='team-img' alt='picture'/>
-                <h3>Anirudha Udupa</h3>
-                <div className='team-info'>
-                  <p>Team Lead</p>
-                  <ul className='team-icon display: inline-flex flex-row'>
-                    <li><a href='#' className='instagram'>
-                    <iframe className='fa fa-instagram'></iframe>
-                    </a></li>
-                    <li><a href='#' className='github'>
-                    <iframe className='fa fa-github'></iframe>
-                    </a></li>
-                    <li><a href='#'className='linkedin'>
-                    <iframe className='fa fa-linkedin'></iframe>
-                    </a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-=======
-    <div className="h-dvh bg-black bg-cover">
+    <div className="h-lvh bg-black bg-cover">
       <div className="col-md-15 text-center">
         <br />
         <h2 className="font-bold text-white text-8xl  text-s letter-spacing tracking-wide">
@@ -119,8 +24,8 @@ export default function About() {
         </p>
       </div>
       <br />
-      <div className="flex gap-4 place-content-center ">
-        <div className="col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 w-80 h-auto rounded-lg p-8 gap-4">
+      <div className="flex gap-9 place-content-center ">
+        <div className="col-sm-6 col-md-4 transition ease-in-out delay-150 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-white-500 duration-300 w-auto h-auto rounded-lg p-8 gap-4">
           <div className="team-item ">
             <img
               src="/Anirudh-min.jpeg"
@@ -133,12 +38,11 @@ export default function About() {
             </h3>
             <div className="team-info">
               <p className="text-center text-1xl text-black-400 ">Team Lead</p>
-              <ul className="team-icon rounded-lg flex-1">
+              <ul className="bg-black">
                 <li>
-                  <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                  <i icon="fa fa-instagram"></i>
                 </li>
               </ul>
->>>>>>> fa36536c3714a7ae2a26fb49ac358f4ce936a920
             </div>
           </div>
         </div>
