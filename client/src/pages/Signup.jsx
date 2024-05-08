56d3abdcdd8f51cf98a4b4c74054de4a1a899c67
Krishnas-world <krishnapallan128@gmail.com>
2024-05-09 04:56:34 +0530

    added the signin and auth controller

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -30,7 +30,7 @@ export default function SignUp() {
       }
       setLoading(false);
       if (res.status === 200) {
-        navigate('/sign-in');
+        navigate('/');
       }
     } catch (error) {
       setErrorMessage(error.message);
@@ -83,7 +83,7 @@ export default function SignUp() {
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>Have an account?</span>
-          <Link to='/sign-in' className='text-blue-500'>
+          <Link to='/signin' className='text-blue-500'>
             Sign In
           </Link>
         </div>
