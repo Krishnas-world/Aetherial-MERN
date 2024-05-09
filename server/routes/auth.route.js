de3677a8d6e5d01d00ba66d4614a3a5abe1aaed7
Krishnas-world <krishnapallan128@gmail.com>
2024-05-09 22:56:49 +0530

    Added Google auth and changed the user schema

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const {signup,signin} = require('../controllers/auth.controller.js'); //! Call for the logic part
+const {signup,signin, google} = require('../controllers/auth.controller.js'); //! Call for the logic part
 const router = express.Router();
 
 router.post('/signup',signup) //! Give a route and logic for the same
 router.post('/signin',signin) //! Give a route and logic for the same
-
+router.post('/google',google)
 module.exports = router;
